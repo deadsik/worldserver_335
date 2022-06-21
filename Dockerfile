b11cec27368c97f98da4e446265d20797b60b1f2
deadsik <deadsik@ukr.net>
2022-06-21 19:41:25 +0000

    first commit

---
+++ Dockerfile
@@ -0,0 +1,68 @@
+FROM ubuntu:20.04 
+MAINTAINER admin <evgeniy@kolesnyk.ru> 
+
+ENV WORLDSERVER_TZ="Etc/UTC" \
+    WORLDSERVER_SYSTEM_CORES="8" \
+    WORLDSERVER_SYSTEM_USER="server" \
+    DEFAULT_WORLDSERVER_WORLDSERVERPORT="8085" \
+    DEFAULT_WORLDSERVER_BINDIP="0.0.0.0" \
+    DEFAULT_WORLDSERVER_RATES="1" \
+    DEFAULT_WORLDSERVER_NAME="Trinity" \
+    DEFAULT_WORLDSERVER_MYSQL_AUTOCONF=true \
+    DEFAULT_AUTHSERVER_MYSQL_HOST="127.0.0.1" \
+    DEFAULT_AUTHSERVER_MYSQL_PORT="3306" \
+    DEFAULT_AUTHSERVER_MYSQL_USER="trinity" \
+    DEFAULT_AUTHSERVER_MYSQL_PASSWORD="trinity" \
+    DEFAULT_AUTHSERVER_MYSQL_DB="auth" \
+    DEFAULT_WORLDSERVER_MYSQL_HOST="127.0.0.1" \
+    DEFAULT_WORLDSERVER_MYSQL_PORT="3306" \
+    DEFAULT_WORLDSERVER_MYSQL_USER="trinity" \
+    DEFAULT_WORLDSERVER_MYSQL_PASSWORD="trinity" \
+    DEFAULT_WORLDSERVER_MYSQL_DB="world" \
+    DEFAULT_CHARACTERSSERVER_MYSQL_HOST="127.0.0.1" \
+    DEFAULT_CHARACTERSSERVER_MYSQL_PORT="3306" \
+    DEFAULT_CHARACTERSSERVER_MYSQL_USER="trinity" \
+    DEFAULT_CHARACTERSSERVER_MYSQL_PASSWORD="trinity" \
+    DEFAULT_CHARACTERSSERVER_MYSQL_DB="characters"
+
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN ln -snf /usr/share/zoneinfo/$WORLDSERVER_TZ /etc/localtime && echo $WORLDSERVER_TZ > /etc/timezone && \
+    apt-get update && apt-get install -y apt-utils && \
+    apt-get upgrade -y && \
+    apt-get install build-essential autoconf libtool gcc g++ make git-core wget p7zip-full libncurses5-dev zlib1g-dev libbz2-dev openssl libssl-dev libreadline6-dev libboost-dev libboost-thread-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-iostreams-dev libzmq3-dev libmysqlclient-dev libmysql++-dev curl mysql-client sudo -y
+
+RUN curl -o /root/cmake-3.23.0-rc2.tar.gz https://cmake.org/files/v3.23/cmake-3.23.0-rc2.tar.gz && \
+    cd /root && tar xzf cmake-3.23.0-rc2.tar.gz && \
+    cd cmake-3.23.0-rc2 && \
+    ./configure && \
+    make -j $WORLDSERVER_SYSTEM_CORES && \
+    make install && \
+    rm -rf /root/cmake-3.23.0-rc2 && \
+    rm -f /root/cmake-3.23.0-rc2.tar.gz
+
+RUN useradd -ms /bin/bash $WORLDSERVER_SYSTEM_USER && \
+    mkdir -p /home/$WORLDSERVER_SYSTEM_USER/wow && \
+    mkdir -p /home/$WORLDSERVER_SYSTEM_USER/source && \
+    cd /home/$WORLDSERVER_SYSTEM_USER/source && \
+    git clone https://github.com/TrinityCore/TrinityCore.git && \
+    mkdir -p /home/$WORLDSERVER_SYSTEM_USER/source/TrinityCore/build && \
+    cd /home/$WORLDSERVER_SYSTEM_USER/source/TrinityCore && \
+    git checkout -b 3.3.5 origin/3.3.5 && \
+    cd /home/$WORLDSERVER_SYSTEM_USER/source/TrinityCore/build && \
+    cmake ../ -DCMAKE_INSTALL_PREFIX=/home/$WORLDSERVER_SYSTEM_USER/wow && \
+    make -j $WORLDSERVER_SYSTEM_CORES && \
+    make install
+
+RUN cd /home/$WORLDSERVER_SYSTEM_USER/wow/bin/ && rm -f mapextractor mmaps_generator vmap4assembler vmap4extractor authserver && \
+    rm -f /home/$WORLDSERVER_SYSTEM_USER/wow/etc/authserver.conf.dist && \
+    mv /home/$WORLDSERVER_SYSTEM_USER/wow/etc/worldserver.conf.dist /home/$WORLDSERVER_SYSTEM_USER/wow/etc/worldserver.conf && \
+    curl -o /home/$WORLDSERVER_SYSTEM_USER/wow/bin/maps.tar.gz https://download.piratewow.com/servers/wow335a/maps.tar.gz && \
+    cd /home/$WORLDSERVER_SYSTEM_USER/wow/bin/ && tar xzf maps.tar.gz && rm -f maps.tar.gz && \
+    chown -R $WORLDSERVER_SYSTEM_USER:$WORLDSERVER_SYSTEM_USER /home/$WORLDSERVER_SYSTEM_USER/
+
+ADD entrypoint.sh /
+
+EXPOSE 8085
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+set -e
+
+file_env() {
+    local var="$1"
+    local fileVar="${var}_FILE"
+    local def="${2:-}"
+    if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+        echo "Both $var and $fileVar are set (but are exclusive)"
+        exit 1
+    fi
+    local val="$def"
+    if [ "${!var:-}" ]; then
+        val="${!var}"
+    elif [ "${!fileVar:-}" ]; then
+        val="$(< "${!fileVar}")"
+    fi
+    export "$var"="$val"
+    unset "$fileVar"
+}
+
+# Loads various settings that are used elsewhere in the script
+docker_setup_env() {
+    # Initialize values that might be stored in a file
+    file_env 'WORLDSERVER_WORLDSERVERPORT' $DEFAULT_WORLDSERVER_WORLDSERVERPORT
+    file_env 'WORLDSERVER_BINDIP' $DEFAULT_WORLDSERVER_BINDIP
+    file_env 'WORLDSERVER_RATES' $DEFAULT_WORLDSERVER_RATES
+    file_env 'WORLDSERVER_NAME' $DEFAULT_WORLDSERVER_NAME
+    file_env 'WORLDSERVER_MYSQL_AUTOCONF' $DEFAULT_WORLDSERVER_MYSQL_AUTOCONF
+
+    file_env 'AUTHSERVER_MYSQL_HOST' $DEFAULT_AUTHSERVER_MYSQL_HOST
+    file_env 'AUTHSERVER_MYSQL_PORT' $DEFAULT_AUTHSERVER_MYSQL_PORT
+    file_env 'AUTHSERVER_MYSQL_USER' $DEFAULT_AUTHSERVER_MYSQL_USER
+    file_env 'AUTHSERVER_MYSQL_PASSWORD' $DEFAULT_AUTHSERVER_MYSQL_PASSWORD
+    file_env 'AUTHSERVER_MYSQL_DB' $DEFAULT_AUTHSERVER_MYSQL_DB
+
+    file_env 'WORLDSERVER_MYSQL_HOST' $DEFAULT_WORLDSERVER_MYSQL_HOST
+    file_env 'WORLDSERVER_MYSQL_PORT' $DEFAULT_WORLDSERVER_MYSQL_PORT
+    file_env 'WORLDSERVER_MYSQL_USER' $DEFAULT_WORLDSERVER_MYSQL_USER
+    file_env 'WORLDSERVER_MYSQL_PASSWORD' $DEFAULT_WORLDSERVER_MYSQL_PASSWORD
+    file_env 'WORLDSERVER_MYSQL_DB' $DEFAULT_WORLDSERVER_MYSQL_DB
+
+    file_env 'CHARACTERSSERVER_MYSQL_HOST' $DEFAULT_CHARACTERSSERVER_MYSQL_HOST
+    file_env 'CHARACTERSSERVER_MYSQL_PORT' $DEFAULT_CHARACTERSSERVER_MYSQL_PORT
+    file_env 'CHARACTERSSERVER_MYSQL_USER' $DEFAULT_CHARACTERSSERVER_MYSQL_USER
+    file_env 'CHARACTERSSERVER_MYSQL_PASSWORD' $DEFAULT_CHARACTERSSERVER_MYSQL_PASSWORD
+    file_env 'CHARACTERSSERVER_MYSQL_DB' $DEFAULT_CHARACTERSSERVER_MYSQL_DB
+}
+
+docker_setup_env
+
+
+if $WORLDSERVER_MYSQL_AUTOCONF ; then
+# Set MYSQL Credentials in worldserver.conf
+  sed -r -i 's/^LoginDatabaseInfo     = .*$/LoginDatabaseInfo     = "'${AUTHSERVER_MYSQL_HOST}';'${AUTHSERVER_MYSQL_PORT}';'${AUTHSERVER_MYSQL_USER}';'${AUTHSERVER_MYSQL_PASSWORD}';'${AUTHSERVER_MYSQL_DB}'"/' /home/server/wow/etc/worldserver.conf
+  sed -r -i 's/^WorldDatabaseInfo     = .*$/WorldDatabaseInfo     = "'${WORLDSERVER_MYSQL_HOST}';'${WORLDSERVER_MYSQL_PORT}';'${WORLDSERVER_MYSQL_USER}';'${WORLDSERVER_MYSQL_PASSWORD}';'${WORLDSERVER_MYSQL_DB}'"/' /home/server/wow/etc/worldserver.conf
+  sed -r -i 's/^CharacterDatabaseInfo = .*$/CharacterDatabaseInfo = "'${CHARACTERSSERVER_MYSQL_HOST}';'${CHARACTERSSERVER_MYSQL_PORT}';'${CHARACTERSSERVER_MYSQL_USER}';'${CHARACTERSSERVER_MYSQL_PASSWORD}';'${CHARACTERSSERVER_MYSQL_DB}'"/' /home/server/wow/etc/worldserver.conf
+
+  unset -v AUTHSERVER_MYSQL_PASSWORD
+  unset -v WORLDSERVER_MYSQL_PASSWORD
+  unset -v CHARACTERSSERVER_MYSQL_PASSWORD
+fi
+
+sed -r -i 's/^WorldServerPort = .*$/WorldServerPort = '${WORLDSERVER_WORLDSERVERPORT}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^BindIP = .*$/BindIP = '${WORLDSERVER_BINDIP}'/' /home/server/wow/etc/worldserver.conf
+
+sed -r -i 's/^Motd = .*$/Motd = "Welcome to '${WORLDSERVER_NAME}' x'${WORLDSERVER_RATES}' server."/' /home/server/wow/etc/worldserver.conf
+
+sed -r -i 's/^Rate.Health            = .*$/Rate.Health            = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.Mana              = .*$/Rate.Mana              = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.Rage.Income       = .*$/Rate.Rage.Income       = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.Rage.Loss         = .*$/Rate.Rage.Loss         = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.RunicPower.Income = .*$/Rate.RunicPower.Income = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.Rage.Loss         = .*$/Rate.Rage.Loss         = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.RunicPower.Income = .*$/Rate.RunicPower.Income = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.RunicPower.Loss   = .*$/Rate.RunicPower.Loss   = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.Focus             = .*$/Rate.Focus             = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.Energy            = .*$/Rate.Energy            = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.Loyalty           = .*$/Rate.Loyalty           = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.Drop.Item.Poor             = .*$/Rate.Drop.Item.Poor             = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.Drop.Item.Normal           = .*$/Rate.Drop.Item.Normal           = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.Drop.Item.Uncommon         = .*$/Rate.Drop.Item.Uncommon         = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.Drop.Item.Rare             = .*$/Rate.Drop.Item.Rare             = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.Drop.Item.Epic             = .*$/Rate.Drop.Item.Epic             = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.Drop.Item.Legendary        = .*$/Rate.Drop.Item.Legendary        = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.Drop.Item.Artifact         = .*$/Rate.Drop.Item.Artifact         = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.Drop.Item.Referenced       = .*$/Rate.Drop.Item.Referenced       = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.Drop.Money                 = .*$/Rate.Drop.Money                 = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.Drop.Item.ReferencedAmount = .*$/Rate.Drop.Item.ReferencedAmount = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.XP.Kill    = .*$/Rate.XP.Kill    = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.XP.Quest   = .*$/Rate.XP.Quest   = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+sed -r -i 's/^Rate.XP.Explore = .*$/Rate.XP.Explore = '${WORLDSERVER_RATES}'/' /home/server/wow/etc/worldserver.conf
+
+# Run worldserver
+sudo -H -u server bash -c "cd /home/server/wow/bin/ && ./worldserver"
+exec "$@"
